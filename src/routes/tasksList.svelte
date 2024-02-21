1a4b5902ebb07a2cf0b1df5d254d9ee0224cb9bd
Chloé T. <chloe.thomas14@gmail.com>
2024-02-21 16:45:40 +0100

    on:delete refactory

--- src/routes/tasksList.svelte
+++ src/routes/tasksList.svelte
@@ -1,18 +1,11 @@
 <script>
-	import { createEventDispatcher } from 'svelte';
 	import Task from './task.svelte';
 	export let taskList;
-
-	const dispatch = createEventDispatcher();
-
-	function deleteTask (event) {
-		dispatch('delete', event.detail)
-	}
 
 </script>
 
 {#each taskList as task}
-<Task task={task} on:delete={deleteTask} />
+<Task task={task} on:delete />
 {/each}
 
 <!-- {JSON.stringify(task)} -->
